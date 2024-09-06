91986678d6958ae4d3dcf9b2325a4c6766878e0b
jonmun <88200206+jon-mun@users.noreply.github.com>
2024-09-06 14:12:31 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your not changes instantly.</li>
+          <li>Updated List</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
